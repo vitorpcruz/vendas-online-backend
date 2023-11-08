9dfa4cbcd45a606b022321a898e4b4a7ea349d14
Vitor P Cruz <vitor-pcruz@hotmail.com>
2023-11-08 07:37:59 -0300

    feat: start project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port: number = 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+
+bootstrap();
